3f2519631812750b2587ee111abff18a82126b62
francoeurdavid <francoeurdavid@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-30 16:58:07 +0000

    Added the settings for email notification (email and activation field)
    git-svn-id: http://plugins.svn.wordpress.org/cleanbook/trunk@1016847 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/settings.php
+++ admin/settings.php
@@ -17,26 +17,62 @@ function cleanbook_admin_menu()
 */
 function register_settings() {
     //register our settings
-    register_setting( 'cb-settings-group', 'cleanbook_options');
+    register_setting( 'cb-settings-group', 'cleanbook_options', 'validate_options');
+
+    //General settings
     add_settings_section('cleanbook_general', __('General Settings', 'cleanbook'), 'general_settings', 'cleanbook_general_page');
     add_settings_field('auto_register_bootstrap', __('Auto-register Bootstrap', 'cleanbook'), 'auto_register_bootstrap_string', 'cleanbook_general_page', 'cleanbook_general');
+
+    //General settings
+    add_settings_section('cleanbook_email', __('Email Notification Settings', 'cleanbook'), 'email_settings', 'cleanbook_general_page');
+    add_settings_field('activate_email_notification', __('Activate email notification', 'cleanbook'), 'email_activation_string', 'cleanbook_general_page', 'cleanbook_email');
+    add_settings_field('email', __('Email to notify', 'cleanbook'), 'email_string', 'cleanbook_general_page', 'cleanbook_email');
 }
 
 /*
-	Print out a description of the general settings page
+    Print out a description of the general settings section
 */
 function general_settings(){
-    echo "<p>" . _e("This is where you will find all of the general settings 
-                    that affects the behavior of your Cleanbook plugin.", 'cleanbook') . "</p>";
+    echo "<p>" . __("This is where you will find all of the general settingsthat affects the behavior of your Cleanbook plugin.", 'cleanbook') . "</p>";
 }
-
+/*
+    Prints out Auto register option
+*/
 function auto_register_bootstrap_string() {
     $options = get_option('cleanbook_options');
 
     echo "<input id='auto_register_bootstrap' name='cleanbook_options[auto_register_bootstrap]' 
-             type='checkbox' value='1'" . checked(1, $options['auto_register_bootstrap'], false ) . "' />";
+             type='checkbox' value='1'" . checked($options['auto_register_bootstrap'], 1, false ) . "' />";
 }
 
+/*
+    Print out a description of the email notification
+*/
+function email_settings(){
+    echo "<p>" . __("This is where you will find all of the settings regarding notification by email.", 'cleanbook') . "</p>";
+}
+/*
+    Prints out function for email settings
+*/
+function email_activation_string() {
+    $options = get_option('cleanbook_options');
+
+    echo "<input id='activate_email_notification' name='cleanbook_options[activate_email_notification]' 
+             type='checkbox' value='1'" . checked($options['activate_email_notification'], 1, false ) . "' />";
+}
+
+function email_string() {
+    $options = get_option('cleanbook_options');
+
+    $isActivated = $options['activate_email_notification'];
+
+    echo "<input id='email' name='cleanbook_options[email]' 
+             type='text' value='{$options['email']}' ". disabled( !$isActivated, true, false) . " />";
+}
+
+/*
+    Validate the settings
+*/
 function validate_options($input){
     $newinput['auto_register_bootstrap'] = trim($input['auto_register_bootstrap']);
     
@@ -45,7 +81,7 @@ function validate_options($input){
         $newinput['auto_register_bootstrap'] = 0;
     }
     
-    return $newinput;
+    return $input;
 }
 
 
