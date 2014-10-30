946863c8621c4cd779d97e92ab49453c38fab0ce
francoeurdavid <francoeurdavid@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-30 16:56:53 +0000

    Formatting in install and listdata Added settings.php Added a settings group and a settings section. Added a setting field
    git-svn-id: http://plugins.svn.wordpress.org/cleanbook/trunk@1016840 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/listdata.php
+++ admin/listdata.php
@@ -6,57 +6,58 @@ global $wpdb;
 $appointments = $wpdb->prefix . CLEANBOOK_TABLE_APPOINTMENTS;
 $results = $wpdb->get_results("SELECT * FROM $appointments" , ARRAY_A); 
 ?>
-<div class="loading">
-	<div class="loader"></div>
-</div>
+<div class="wrap">
+	<div class="loading">
+		<div class="loader"></div>
+	</div>
 
-<h2><?php echo $translated_labels['listing-title']; ?></h2>
+	<h2><?php echo $translated_labels['listing-title']; ?></h2>
 
-<div id="appointments">
-	<table rules="groups">
-		<thead  style="border-collapse: collapse;" >
-			<tr>
-				<th><?php echo $translated_labels['id']; ?></th>
-				<th><?php echo $translated_labels['name']; ?></th>
-				<th><?php echo $translated_labels['email']; ?></th>
-				<th><?php echo $translated_labels['phone']; ?></th>
-				<th><?php echo $translated_labels['comment']; ?></th>
-				<th><?php echo $translated_labels['datetime']; ?></th>
-				<th></th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php
-			foreach($results AS $key=>$appointment){ 
-				?>
-				<tr id="appointment-<?php echo $appointment['id']; ?>" 
-					class="<?php echo $key % 2 == 0 ? 'even' : 'uneven'; ?>">
-					<td class="app-id"><?php echo $appointment['id']; ?></td>
-					<td class="app-name"><?php echo $appointment['name']; ?></td>
-					<td class="app-email"><?php echo $appointment['email']; ?></td>
-					<td class="app-phone"><?php echo $appointment['phone']; ?></td>
-					<td class="app-comment"><?php echo $appointment['comment']; ?></td>
-					<td class="app-datetime"><?php echo $appointment['datetime']; ?></td>
-					<?php
-					$active_label = $appointment['active'] ? 
-					$translated_labels['deactivate'] : 
-					$translated_labels['activate'];
+	<div id="appointments">
+		<table rules="groups">
+			<thead  style="border-collapse: collapse;" >
+				<tr>
+					<th><?php echo $translated_labels['id']; ?></th>
+					<th><?php echo $translated_labels['name']; ?></th>
+					<th><?php echo $translated_labels['email']; ?></th>
+					<th><?php echo $translated_labels['phone']; ?></th>
+					<th><?php echo $translated_labels['comment']; ?></th>
+					<th><?php echo $translated_labels['datetime']; ?></th>
+					<th></th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+				foreach($results AS $key=>$appointment){ 
 					?>
-					<td class="app-active">
-						<input type="checkbox" 
-						<?php if($appointment['active']){ echo 'checked' ;} ?> 
-						data-id="<?php echo $appointment['id']; ?>"
-						/>
-					</a>
-				</td>
-			</tr>
-			<?php
-		}
-		?> 
-	</tbody>
-</table>
+					<tr id="appointment-<?php echo $appointment['id']; ?>" 
+						class="<?php echo $key % 2 == 0 ? 'even' : 'uneven'; ?>">
+						<td class="app-id"><?php echo $appointment['id']; ?></td>
+						<td class="app-name"><?php echo $appointment['name']; ?></td>
+						<td class="app-email"><?php echo $appointment['email']; ?></td>
+						<td class="app-phone"><?php echo $appointment['phone']; ?></td>
+						<td class="app-comment"><?php echo $appointment['comment']; ?></td>
+						<td class="app-datetime"><?php echo $appointment['datetime']; ?></td>
+						<?php
+						$active_label = $appointment['active'] ? 
+						$translated_labels['deactivate'] : 
+						$translated_labels['activate'];
+						?>
+						<td class="app-active">
+							<input type="checkbox" 
+							<?php if($appointment['active']){ echo 'checked' ;} ?> 
+							data-id="<?php echo $appointment['id']; ?>"
+							/>
+						</a>
+					</td>
+				</tr>
+				<?php
+			}
+			?> 
+		</tbody>
+	</table>
+	</div>
 </div>
-
 <script type="text/javascript">
 jQuery(document).ready(function($) { 
 
